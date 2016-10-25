f95f421d4b1a0c32976e729fbd1cabcb17bf547e
Thanh Khac <kingkid162@gmail.com>
2016-10-25 20:28:55 +0700

    milestone 5 tdd sort

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+    it "returns nothing if there are no products" do
+      expect(Product.alphabetical).to eq []
+    end
+
+    it "returns [a] if there is only 1 products" do
+      a = Product.create! name: 'a'
+      expect(Product.alphabetical).to eq [a]
+    end
+    it "should returns [a, b, c] if there are three products" do
+      a = Product.create!(name: 'a')
+      b = Product.create!(name: 'b')
+      c = Product.create!(name: 'c')
+
+      expect(Product.alphabetical).to eq [a,b,c]
+    end
+  end
 end
